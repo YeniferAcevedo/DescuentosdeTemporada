749f9bab13e4be6122bcc56d03ea493bcc610b4a
YeniferAcevedo <yeniferacevedo@hotmail.com>
2020-08-24 22:34:09 -0500

    Programa que permite aplicar descuento según temporada y valor de compra

---
+++ DescuentosdeTemporada/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DescuentosdeTemporada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblValorFinal = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtValorCompra = new System.Windows.Forms.TextBox();
+            this.cmbTemporada = new System.Windows.Forms.ComboBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(270, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DESCUENTOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(285, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el valor de la compra:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(417, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Temporada";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(39, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descuento:";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Font = new System.Drawing.Font("Arial", 14F);
+            this.lblDescuento.Location = new System.Drawing.Point(172, 259);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(21, 22);
+            this.lblDescuento.TabIndex = 4;
+            this.lblDescuento.Text = "0";
+            this.lblDescuento.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(262, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Valor a pagar:";
+            // 
+            // lblValorFinal
+            // 
+            this.lblValorFinal.AutoSize = true;
+            this.lblValorFinal.Font = new System.Drawing.Font("Arial", 14F);
+            this.lblValorFinal.Location = new System.Drawing.Point(406, 259);
+            this.lblValorFinal.Name = "lblValorFinal";
+            this.lblValorFinal.Size = new System.Drawing.Size(21, 22);
+            this.lblValorFinal.TabIndex = 6;
+            this.lblValorFinal.Text = "0";
+            this.lblValorFinal.Visible = false;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCalcular.Location = new System.Drawing.Point(696, 132);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(92, 29);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtValorCompra
+            // 
+            this.txtValorCompra.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtValorCompra.Font = new System.Drawing.Font("Arial", 14F);
+            this.txtValorCompra.Location = new System.Drawing.Point(298, 168);
+            this.txtValorCompra.Name = "txtValorCompra";
+            this.txtValorCompra.Size = new System.Drawing.Size(100, 29);
+            this.txtValorCompra.TabIndex = 8;
+            // 
+            // cmbTemporada
+            // 
+            this.cmbTemporada.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cmbTemporada.Font = new System.Drawing.Font("Arial", 13F);
+            this.cmbTemporada.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.cmbTemporada.FormattingEnabled = true;
+            this.cmbTemporada.Items.AddRange(new object[] {
+            "Escolar",
+            "Halloween",
+            "Diciembre"});
+            this.cmbTemporada.Location = new System.Drawing.Point(542, 167);
+            this.cmbTemporada.Name = "cmbTemporada";
+            this.cmbTemporada.Size = new System.Drawing.Size(121, 27);
+            this.cmbTemporada.TabIndex = 9;
+            this.cmbTemporada.Text = "Seleccionar";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLimpiar.Location = new System.Drawing.Point(696, 195);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(92, 29);
+            this.btnLimpiar.TabIndex = 12;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DescuentosdeTemporada.Properties.Resources._5a1d5e967b1aa2_0647017115118741985042;
+            this.pictureBox2.Location = new System.Drawing.Point(528, 259);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(194, 162);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.cmbTemporada);
+            this.Controls.Add(this.txtValorCompra);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblValorFinal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblDescuento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Descuentos de Temporada";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblValorFinal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtValorCompra;
+        private System.Windows.Forms.ComboBox cmbTemporada;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ DescuentosdeTemporada/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DescuentosdeTemporada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double valorCompra = double.Parse(txtValorCompra.Text);
+            double valorDescuento;
+            double valorFinal;
+            string descuento;
+
+            switch (cmbTemporada.SelectedItem){
+                case "Escolar":
+
+                    if (valorCompra <= 100000){
+                        descuento = "0%";
+                        valorFinal = valorCompra;
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    else if (valorCompra > 100001 && valorCompra <= 300000){
+                        descuento = "10%";
+                        valorDescuento = valorCompra * 0.1 - valorCompra;
+                        valorFinal = valorDescuento * (-1);
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    else if (valorCompra > 300000){
+                        descuento = "15%";
+                        valorDescuento = valorCompra * 0.15 - valorCompra;
+                        valorFinal = valorDescuento * (-1);
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    lblDescuento.Visible = true;
+                    lblValorFinal.Visible = true;
+                    break;
+
+                case "Halloween":
+                    if (valorCompra > 50 && valorCompra <= 30000){
+                        descuento = "0%";
+                        valorFinal = valorCompra;
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));    
+                    }
+                    else if (valorCompra > 30001 && valorCompra <= 200000){
+                        descuento = "2%";
+                        valorDescuento = valorCompra * 0.02 - valorCompra;
+                        valorFinal = valorDescuento * (-1);
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    else if (valorCompra > 200001){
+                        descuento = "5%";
+                        valorDescuento = valorCompra * 0.05 - valorCompra;
+                        valorFinal = valorDescuento * (-1);
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    lblDescuento.Visible = true;
+                    lblValorFinal.Visible = true;
+                    break;
+
+                case "Diciembre":
+                    if (valorCompra > 50 && valorCompra <= 300000){
+                        descuento = "0%";
+                        valorFinal = valorCompra;
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    else if (valorCompra > 300001 && valorCompra <= 500000){
+                        descuento = "5%";
+                        valorDescuento = valorCompra * 0.05 - valorCompra;
+                        valorFinal = valorDescuento * (-1);
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal));
+                    }
+                    else if (valorCompra > 500001){
+                        descuento = "10%";
+                        valorDescuento = valorCompra * 0.1 - valorCompra;
+                        valorFinal = valorDescuento * (-1);
+                        lblDescuento.Text = descuento;
+                        lblValorFinal.Text = Convert.ToString(string.Format("{0:n0}", valorFinal)); 
+                    }
+                    lblDescuento.Visible = true;
+                    lblValorFinal.Visible = true;
+                    break;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbTemporada.SelectedIndex = 0;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtValorCompra.Text = string.Empty;
+            cmbTemporada.Text = string.Empty;
+            lblDescuento.Text = " ";
+            lblValorFinal.Text = " ";
+            txtValorCompra.Focus();
+        }
+    }
+}
+
